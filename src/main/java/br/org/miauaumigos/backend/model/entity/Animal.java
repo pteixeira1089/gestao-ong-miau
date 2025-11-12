083d88458a08785f459b24b6c1924ab5eedd48d6
Pedro Teixeira <pteixeira1089@hotmail.com>
2025-11-12 08:51:51 -0300

    Successfully mapped entities Animal and EventoAnimal to db with annotations

--- src/main/java/br/org/miauaumigos/backend/model/entity/Animal.java
+++ src/main/java/br/org/miauaumigos/backend/model/entity/Animal.java
@@ -1,16 +1,42 @@
 package br.org.miauaumigos.backend.model.entity;
 
 import br.org.miauaumigos.backend.model.enums.EspecieAnimal;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "animal")
 public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 255)
     private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "especie_animal", nullable = false)
     private EspecieAnimal especieAnimal;
+
+    @Column(nullable = false, length = 1)
     private Character sexo;
+
+    @Column(nullable = false)
     private String pelagem;
+
+    @Column(nullable = true, length = 2000)
     private String bio;
+
+    @Column(nullable = true)
     private String urlFoto;
+
+    @OneToMany(mappedBy = "animal", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<EventoAnimal> eventos;
 }
--- src/main/java/br/org/miauaumigos/backend/model/entity/EventoAnimal.java
+++ src/main/java/br/org/miauaumigos/backend/model/entity/EventoAnimal.java
@@ -1,14 +1,35 @@
 package br.org.miauaumigos.backend.model.entity;
 
 import br.org.miauaumigos.backend.model.enums.TipoEventoAnimal;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "evento_animal")
 public class EventoAnimal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "animal_id", nullable = false)
     private Animal animal;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "tipo_evento_animal", nullable = false)
     private TipoEventoAnimal tipo;
-    private LocalDate data;
-    private String descricao;
+
+    @Column(nullable = false)
+    private LocalDate dataEvento;
+
+    @Column
+    private String obsEvento;
 
 }
